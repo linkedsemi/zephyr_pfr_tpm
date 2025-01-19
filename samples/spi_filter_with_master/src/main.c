dda8b9e7475bc9a4ef91d5f8416ef589566f4ccf
Hunter Liu <htliu@linkedsemi.com>
2025-01-19 23:19:32 +0800

    samples/spi_filter_with_master: add some cases

--- samples/spi_filter_with_master/src/main.c
+++ samples/spi_filter_with_master/src/main.c
@@ -55,32 +55,306 @@ void spi_init(void)
     }
 }
 
+uint32_t g_cnt = 0;
+uint8_t g_cmd = 0;
+uint8_t g_test_cmd = 0;
+static void spif_callback(const struct device *dev,
+                          uint32_t callback_idx,
+                          void *user_data,
+                          void *drv_data)
+{
+    ARG_UNUSED(dev);
+    ARG_UNUSED(callback_idx);
+    ARG_UNUSED(user_data);
+    ARG_UNUSED(drv_data);
+
+    printk("g_cnt: %d  g_cmd: %#x  g_test_cmd: %#x\n", g_cnt++, g_cmd, g_test_cmd);
+}
+
+void test_general_cmd(const struct device *const spifilter)
+{
+    for (/*uint8_t*/ g_cmd = 0; g_cmd <= 0xff; g_cmd++) {
+        printf("----------------------------------------------------\n");
+        printf("spif_add_general_cmd: %#x\n", g_cmd);
+        spif_add_general_cmd(spifilter, g_cmd);
+        for (/*uint8_t*/ g_test_cmd = 0; g_test_cmd <= 0xff; g_test_cmd++) {
+            if (HAL_SSI_Transmit(&SsiHandle, &g_test_cmd, 1) != HAL_OK) {
+                /* Transfer error in transmission process */
+                while(1);
+            }
+            if (g_test_cmd == 0xff) {
+                break;
+            }
+        }
+        if (HAL_SSI_Transmit(&SsiHandle, &g_cmd, 1) != HAL_OK) {
+            /* Transfer error in transmission process */
+            while(1);
+        }
+        spif_remove_general_cmd(spifilter, g_cmd);
+        printf("----------------------------------------------------\n");
+        if (g_cmd == 0xff) {
+            break;
+        }
+    }
+}
+
+void test_spicmd_spi3b(const struct device *const spifilter)
+{
+    do {
+        g_cmd = CMD_PAGE_PROGRAM;
+        uint8_t tx_data[] = {g_cmd, 0x1, 0x2, 0x3, 0x4, 0x5a, 0xf7};
+        spif_address_privilege_config(spifilter,
+                                    FLAG_ADDR_PRIV_WRITE_SELECT,
+                                    FLAG_ADDR_PRIV_DISABLE,
+                                    MB(0),
+                                    MB(256));
+        printf("spif_add_cmd: %#x\n", g_cmd);
+        spif_add_cmd(spifilter, g_cmd);
+        if (HAL_SSI_Transmit(&SsiHandle, tx_data, ARRAY_SIZE(tx_data)) != HAL_OK) {
+            /* Transfer error in transmission process */
+            while(1);
+        }
+    } while(0);
+    do {
+        g_cmd = CMD_PAGE_PROGRAM;
+        uint8_t tx_data[] = {g_cmd, 0x1, 0x2, 0x3, 0x4, 0x5a, 0xf7};
+        spif_address_privilege_config(spifilter,
+                                    FLAG_ADDR_PRIV_WRITE_SELECT,
+                                    FLAG_ADDR_PRIV_ENABLE,
+                                    MB(0),
+                                    MB(256));
+        printf("spif_add_cmd: %#x\n", g_cmd);
+        spif_add_cmd(spifilter, g_cmd);
+        if (HAL_SSI_Transmit(&SsiHandle, tx_data, ARRAY_SIZE(tx_data)) != HAL_OK) {
+            /* Transfer error in transmission process */
+            while(1);
+        }
+    } while(0);
+    do {
+        g_cmd = CMD_PAGE_PROGRAM;
+        uint8_t tx_data[] = {g_cmd, 0x1, 0x2, 0x3, 0x4, 0x5a, 0xf7};
+        spif_address_privilege_config(spifilter,
+                                    FLAG_ADDR_PRIV_WRITE_SELECT,
+                                    FLAG_ADDR_PRIV_DISABLE,
+                                    MB(0),
+                                    MB(8));
+        spif_address_privilege_config(spifilter,
+                                    FLAG_ADDR_PRIV_WRITE_SELECT,
+                                    FLAG_ADDR_PRIV_ENABLE,
+                                    MB(8),
+                                    MB(16));
+        printf("spif_add_cmd: %#x\n", g_cmd);
+        spif_add_cmd(spifilter, g_cmd);
+        if (HAL_SSI_Transmit(&SsiHandle, tx_data, ARRAY_SIZE(tx_data)) != HAL_OK) {
+            /* Transfer error in transmission process */
+            while(1);
+        }
+    } while(0);
+    do {
+        g_cmd = CMD_PAGE_PROGRAM;
+        uint8_t tx_data[] = {g_cmd, 0x1, 0x2, 0x3, 0x4, 0x5a, 0xf7};
+        spif_address_privilege_config(spifilter,
+                                    FLAG_ADDR_PRIV_WRITE_SELECT,
+                                    FLAG_ADDR_PRIV_ENABLE,
+                                    MB(0),
+                                    MB(8));
+        spif_address_privilege_config(spifilter,
+                                    FLAG_ADDR_PRIV_WRITE_SELECT,
+                                    FLAG_ADDR_PRIV_DISABLE,
+                                    MB(8),
+                                    MB(16));
+        printf("spif_add_cmd: %#x\n", g_cmd);
+        spif_add_cmd(spifilter, g_cmd);
+        if (HAL_SSI_Transmit(&SsiHandle, tx_data, ARRAY_SIZE(tx_data)) != HAL_OK) {
+            /* Transfer error in transmission process */
+            while(1);
+        }
+    } while(0);
+    do {
+        g_cmd = CMD_PAGE_PROGRAM;
+        uint8_t tx_data[] = {g_cmd, 0x7, 0x2, 0x3, 0x4, 0x5a, 0xf7};
+        spif_address_privilege_config(spifilter,
+                                    FLAG_ADDR_PRIV_WRITE_SELECT,
+                                    FLAG_ADDR_PRIV_DISABLE,
+                                    MB(0),
+                                    MB(8));
+        spif_address_privilege_config(spifilter,
+                                    FLAG_ADDR_PRIV_WRITE_SELECT,
+                                    FLAG_ADDR_PRIV_ENABLE,
+                                    MB(8),
+                                    MB(16));
+        printf("spif_add_cmd: %#x\n", g_cmd);
+        spif_add_cmd(spifilter, g_cmd);
+        if (HAL_SSI_Transmit(&SsiHandle, tx_data, ARRAY_SIZE(tx_data)) != HAL_OK) {
+            /* Transfer error in transmission process */
+            while(1);
+        }
+    } while(0);
+    do {
+        g_cmd = CMD_PAGE_PROGRAM;
+        uint8_t tx_data[] = {g_cmd, 0x7, 0x2, 0x3, 0x4, 0x5a, 0xf7};
+        spif_address_privilege_config(spifilter,
+                                    FLAG_ADDR_PRIV_WRITE_SELECT,
+                                    FLAG_ADDR_PRIV_ENABLE,
+                                    MB(0),
+                                    MB(8));
+        spif_address_privilege_config(spifilter,
+                                    FLAG_ADDR_PRIV_WRITE_SELECT,
+                                    FLAG_ADDR_PRIV_DISABLE,
+                                    MB(8),
+                                    MB(16));
+        printf("spif_add_cmd: %#x\n", g_cmd);
+        spif_add_cmd(spifilter, g_cmd);
+        if (HAL_SSI_Transmit(&SsiHandle, tx_data, ARRAY_SIZE(tx_data)) != HAL_OK) {
+            /* Transfer error in transmission process */
+            while(1);
+        }
+    } while(0);
+}
+
+void test_spicmd_spi4b(const struct device *const spifilter)
+{
+    do {
+        g_cmd = CMD_4BYTE_MODE_ENTER;
+        printf("spif_add_cmd: %#x\n", g_cmd);
+        spif_add_cmd(spifilter, g_cmd);
+        if (HAL_SSI_Transmit(&SsiHandle, &g_cmd, 1) != HAL_OK) {
+            /* Transfer error in transmission process */
+            while(1);
+        }
+    } while(0);
+    do {
+        g_cmd = CMD_4BYTE_PAGE_PROGRAM;
+        uint8_t tx_data[] = {g_cmd, 0x1, 0x2, 0x3, 0x4, 0x5a, 0xf7};
+        spif_address_privilege_config(spifilter,
+                                    FLAG_ADDR_PRIV_WRITE_SELECT,
+                                    FLAG_ADDR_PRIV_DISABLE,
+                                    MB(0),
+                                    MB(256));
+        printf("spif_add_cmd: %#x\n", g_cmd);
+        spif_add_cmd(spifilter, g_cmd);
+        if (HAL_SSI_Transmit(&SsiHandle, tx_data, ARRAY_SIZE(tx_data)) != HAL_OK) {
+            /* Transfer error in transmission process */
+            while(1);
+        }
+    } while(0);
+    do {
+        g_cmd = CMD_4BYTE_PAGE_PROGRAM;
+        uint8_t tx_data[] = {g_cmd, 0x1, 0x2, 0x3, 0x4, 0x5a, 0xf7};
+        spif_address_privilege_config(spifilter,
+                                    FLAG_ADDR_PRIV_WRITE_SELECT,
+                                    FLAG_ADDR_PRIV_ENABLE,
+                                    MB(0),
+                                    MB(256));
+        printf("spif_add_cmd: %#x\n", g_cmd);
+        spif_add_cmd(spifilter, g_cmd);
+        if (HAL_SSI_Transmit(&SsiHandle, tx_data, ARRAY_SIZE(tx_data)) != HAL_OK) {
+            /* Transfer error in transmission process */
+            while(1);
+        }
+    } while(0);
+    do {
+        g_cmd = CMD_4BYTE_PAGE_PROGRAM;
+        uint8_t tx_data[] = {g_cmd, 0x1, 0x2, 0x3, 0x4, 0x5a, 0xf7};
+        spif_address_privilege_config(spifilter,
+                                    FLAG_ADDR_PRIV_WRITE_SELECT,
+                                    FLAG_ADDR_PRIV_DISABLE,
+                                    MB(0),
+                                    MB(128));
+        spif_address_privilege_config(spifilter,
+                                    FLAG_ADDR_PRIV_WRITE_SELECT,
+                                    FLAG_ADDR_PRIV_ENABLE,
+                                    MB(128),
+                                    MB(256));
+        printf("spif_add_cmd: %#x\n", g_cmd);
+        spif_add_cmd(spifilter, g_cmd);
+        if (HAL_SSI_Transmit(&SsiHandle, tx_data, ARRAY_SIZE(tx_data)) != HAL_OK) {
+            /* Transfer error in transmission process */
+            while(1);
+        }
+    } while(0);
+    do {
+        g_cmd = CMD_4BYTE_PAGE_PROGRAM;
+        uint8_t tx_data[] = {g_cmd, 0x1, 0x2, 0x3, 0x4, 0x5a, 0xf7};
+        spif_address_privilege_config(spifilter,
+                                    FLAG_ADDR_PRIV_WRITE_SELECT,
+                                    FLAG_ADDR_PRIV_ENABLE,
+                                    MB(0),
+                                    MB(128));
+        spif_address_privilege_config(spifilter,
+                                    FLAG_ADDR_PRIV_WRITE_SELECT,
+                                    FLAG_ADDR_PRIV_DISABLE,
+                                    MB(128),
+                                    MB(256));
+        printf("spif_add_cmd: %#x\n", g_cmd);
+        spif_add_cmd(spifilter, g_cmd);
+        if (HAL_SSI_Transmit(&SsiHandle, tx_data, ARRAY_SIZE(tx_data)) != HAL_OK) {
+            /* Transfer error in transmission process */
+            while(1);
+        }
+    } while(0);
+    do {
+        g_cmd = CMD_4BYTE_PAGE_PROGRAM;
+        uint8_t tx_data[] = {g_cmd, 0x7, 0x2, 0x3, 0x4, 0x5a, 0xf7};
+        spif_address_privilege_config(spifilter,
+                                    FLAG_ADDR_PRIV_WRITE_SELECT,
+                                    FLAG_ADDR_PRIV_DISABLE,
+                                    MB(0),
+                                    MB(128));
+        spif_address_privilege_config(spifilter,
+                                    FLAG_ADDR_PRIV_WRITE_SELECT,
+                                    FLAG_ADDR_PRIV_ENABLE,
+                                    MB(128),
+                                    MB(256));
+        printf("spif_add_cmd: %#x\n", g_cmd);
+        spif_add_cmd(spifilter, g_cmd);
+        if (HAL_SSI_Transmit(&SsiHandle, tx_data, ARRAY_SIZE(tx_data)) != HAL_OK) {
+            /* Transfer error in transmission process */
+            while(1);
+        }
+    } while(0);
+    do {
+        g_cmd = CMD_4BYTE_PAGE_PROGRAM;
+        uint8_t tx_data[] = {g_cmd, 0x7, 0x2, 0x3, 0x4, 0x5a, 0xf7};
+        spif_address_privilege_config(spifilter,
+                                    FLAG_ADDR_PRIV_WRITE_SELECT,
+                                    FLAG_ADDR_PRIV_ENABLE,
+                                    MB(0),
+                                    MB(128));
+        spif_address_privilege_config(spifilter,
+                                    FLAG_ADDR_PRIV_WRITE_SELECT,
+                                    FLAG_ADDR_PRIV_DISABLE,
+                                    MB(128),
+                                    MB(256));
+        printf("spif_add_cmd: %#x\n", g_cmd);
+        spif_add_cmd(spifilter, g_cmd);
+        if (HAL_SSI_Transmit(&SsiHandle, tx_data, ARRAY_SIZE(tx_data)) != HAL_OK) {
+            /* Transfer error in transmission process */
+            while(1);
+        }
+    } while(0);
+}
+
 int main(void)
 {
     const struct device *const spifilter = DEVICE_DT_GET(DT_ALIAS(spifilter));
-    uint8_t test_cmd = 0x5a; //0xce
-    uint8_t chip_erase = 0xce;
-    uint8_t cmd_read = CMD_READ;
-
-    // spif_dump_cmd_table(spifilter);
-    // spif_dump_rw_addr_privilege_table(spifilter);
-    spif_add_cmd(spifilter, CMD_READ);
-    // spif_dump_cmd_table(spifilter);
-    // spif_dump_rw_addr_privilege_table(spifilter);
-
+    int ret = linkedsemi_spif_register_callback(spifilter, 0, spif_callback, NULL);
+    if (ret) {
+        return -1;
+    }
     spi_init();
-    if (HAL_SSI_Transmit(&SsiHandle, &test_cmd, 1) != HAL_OK) {
-        /* Transfer error in transmission process */
-        while(1);
-    }
-    if (HAL_SSI_Transmit(&SsiHandle, &chip_erase, 1) != HAL_OK) {
-        /* Transfer error in transmission process */
-        while(1);
-    }
-    if (HAL_SSI_Transmit(&SsiHandle, &cmd_read, 1) != HAL_OK) {
-        /* Transfer error in transmission process */
-        while(1);
-    }
+
+    test_spicmd_spi3b(spifilter);
+    test_spicmd_spi4b(spifilter);
+#if 0
+    test_spicmd_qpi3b(spifilter); //TBD
+    test_spicmd_qpi4b(spifilter); //TBD
+    test_qpi_3b(spifilter); //TBD
+    test_qpi_4b(spifilter); //TBD
+#endif
+    test_general_cmd(spifilter);
+
+    printf("done\n");
     while(1);
 
     return 0;
