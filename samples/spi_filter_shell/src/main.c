dbf2cacdf60c39f55049144136cdbfe946d7626c
Hunter Liu <htliu@linkedsemi.com>
2025-01-21 15:05:18 +0800

    samples/spi_filter_shell: new sample

---
+++ samples/spi_filter_shell/src/main.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2024 Linkedsemi
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#if !defined(CONFIG_SPI_FILTER_LINKEDSEMI)
+    #error no CONFIG_SPI_FILTER_LINKEDSEMI define
+#endif
+
+#if !defined(CONFIG_SPI_FILTER_LINKEDSEMI_SHELL)
+    #error no CONFIG_SPI_FILTER_LINKEDSEMI_SHELL define
+#endif
+
+#include <zephyr/drivers/spi.h>
+#include <zephyr/kernel.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <zephyr/kernel.h>
+#include <reg_spi_filter.h>
+#include <spi_filter.h>
+
+int main(void)
+{
+    // const struct device *const spifilter = DEVICE_DT_GET(DT_ALIAS(spifilter));
+
+    // spif_dump_cmd_table(spifilter);
+    // spif_dump_rw_addr_privilege_table(spifilter);
+    // spif_add_cmd(spifilter, CMD_READ);
+    // spif_dump_cmd_table(spifilter);
+    // spif_dump_rw_addr_privilege_table(spifilter);
+
+    return 0;
+}
