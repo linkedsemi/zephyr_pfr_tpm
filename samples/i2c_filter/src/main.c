1f0a8079190b8fde12d6d64e2ff123d87dfdef05
Hunter Liu <htliu@linkedsemi.com>
2025-01-23 14:10:36 +0800

    samples/i2c_filter: add linkedsemi_i2c_filter_dump_bitmap()

--- samples/i2c_filter/src/main.c
+++ samples/i2c_filter/src/main.c
@@ -8,6 +8,7 @@
     #error no CONFIG_I2C_FILTER_LINKEDSEMI define
 #endif
 
+#include <string.h>
 #include <zephyr/kernel.h>
 #include <i2c_filter.h>
 
@@ -15,7 +16,17 @@ int main(void)
 {
     const struct device *const i2cfilter = DEVICE_DT_GET(DT_ALIAS(i2cfilter));
     uint32_t bitmap[LINKEDSEMI_I2C_F_REMAP_SIZE_U32] = {};
-    linkedsemi_i2c_filter_fill_bitmap(i2cfilter, 0, 0x50, bitmap);
+    uint32_t dump_bitmap[LINKEDSEMI_I2C_F_REMAP_SIZE_U32] = {};
+    uint8_t index = 0;
+    uint8_t addr = 0x50;
+    uint8_t dump_addr = 0;
+    uint8_t bit = 8;
+    sys_bitfield_set_bit((mem_addr_t)bitmap, bit);
+    linkedsemi_i2c_filter_fill_bitmap(i2cfilter, index, addr, bitmap);
+    linkedsemi_i2c_filter_dump_bitmap(i2cfilter, index, &dump_addr, dump_bitmap);
+    int ret = memcmp(bitmap, dump_bitmap, LINKEDSEMI_I2C_F_REMAP_SIZE_BYTE);
+    __ASSERT_NO_MSG(ret == 0);
+    __ASSERT_NO_MSG(addr == dump_addr);
     linkedsemi_i2c_filter_en(i2cfilter, true, true, false);
 
     return 0;
