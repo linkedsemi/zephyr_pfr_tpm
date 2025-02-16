ebe164f50571272c8a0f142968d1346baebe09af
Hunter Liu <htliu@linkedsemi.com>
2025-02-16 23:04:53 +0800

    spi_filter.c: add spif_addr_privilege_clear_all()

--- drivers/pfr/spi/spi_filter.c
+++ drivers/pfr/spi/spi_filter.c
@@ -466,6 +466,37 @@ end:
     return ret;
 }
 
+void spif_addr_privilege_clear_all(const struct device *dev)
+{
+    __unused const struct linkedsemi_spi_filter_config *dev_config = dev->config;
+    __unused struct linkedsemi_spi_filter_data *dev_data = dev->data;
+
+    for (uint32_t off = 0; off < SPIF_ADDR_SIZE; off += 4) {
+        sys_write32(0, dev_config->base + SPIF_WRITE_ADDR_VALID_EN_ADDR + off); /* memset WRITE_ADDR */
+        sys_write32(0, dev_config->base + SPIF_READ_ADDR_VALID_EN_ADDR + off);  /* memset READ_ADDR */
+    }
+}
+
+void spif_read_addr_privilege_clear_all(const struct device *dev)
+{
+    __unused const struct linkedsemi_spi_filter_config *dev_config = dev->config;
+    __unused struct linkedsemi_spi_filter_data *dev_data = dev->data;
+
+    for (uint32_t off = 0; off < SPIF_ADDR_SIZE; off += 4) {
+        sys_write32(0, dev_config->base + SPIF_READ_ADDR_VALID_EN_ADDR + off);  /* memset READ_ADDR */
+    }
+}
+
+void spif_write_addr_privilege_clear_all(const struct device *dev)
+{
+    __unused const struct linkedsemi_spi_filter_config *dev_config = dev->config;
+    __unused struct linkedsemi_spi_filter_data *dev_data = dev->data;
+
+    for (uint32_t off = 0; off < SPIF_ADDR_SIZE; off += 4) {
+        sys_write32(0, dev_config->base + SPIF_WRITE_ADDR_VALID_EN_ADDR + off); /* memset WRITE_ADDR */
+    }
+}
+
 int spif_add_general_cmd(const struct device *dev, uint8_t cmd)
 {
     __unused const struct linkedsemi_spi_filter_config *dev_config = dev->config;
--- drivers/pfr/spi/spi_filter.h
+++ drivers/pfr/spi/spi_filter.h
@@ -86,6 +86,9 @@ void spif_3byte_mode_config(const struct device *dev);
 void spif_4byte_mode_config(const struct device *dev);
 uint8_t spif_addr_mode_peek(const struct device *dev);
 spif_dma_data_t *spif_log_dma_buf(const struct device *dev);
+void spif_addr_privilege_clear_all(const struct device *dev);
+void spif_read_addr_privilege_clear_all(const struct device *dev);
+void spif_write_addr_privilege_clear_all(const struct device *dev);
 
 #include "ls_hal_dmacv3.h"
 void spif_dma_config(const struct device *dev, DMA_Controller_HandleTypeDef *dmac_inst);
