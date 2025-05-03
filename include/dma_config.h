1f5cce465580803482a1031e3cb3f07e1b6436d6
Hunter Liu <htliu@linkedsemi.com>
2025-05-03 18:45:37 +0800

    lsqsh_pinctrl_pfr_tpm.dtsi: update dmac to zephyr api

---
+++ include/dma_config.h
@@ -0,0 +1,10 @@
+
+#ifndef PFR_TPM_INCLUDE_DMA_CONFIG_H_
+#define PFR_TPM_INCLUDE_DMA_CONFIG_H_
+
+#define DMA_SPIFILTER1                (174)
+#define DMA_SPIFILTER2                (175)
+#define DMA_SPIFILTER3                (176)
+#define DMA_SPIFILTER4                (177)
+
+#endif /* PFR_TPM_INCLUDE_DMA_CONFIG_H_ */
